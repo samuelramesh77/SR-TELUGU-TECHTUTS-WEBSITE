8d81c94867a5adb8fc0ba3f29714a98433a5aae4
samuelramesh77 <samuelramesh77@gmail.com>
2026-08-24 00:58:01 +0530

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+ document.addEventListener("DOMContentLoaded", function () {
     console.log("SR TELUGU TECHTUTS Website Loaded Successfully!");
 });
 
