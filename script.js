57b589b2c661311ea89bcedc97eb1f25ef8cbf4e
samuelramesh77 <samuelramesh77@gmail.com>
2026-08-27 18:38:21 +0530

    Refactor script.js to improve readability

--- script.js
+++ script.js
@@ -277,7 +277,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // =====================================
 
 const YOUTUBE_API_KEY =
-    "AIzaSyCyVhpu_RwHpawkAuhZkTDKvRdHuPzT0YA";
+    "AIzaSyBdU7t7lVR8dKmB6ETIPfr5TVQirI0WEys";
 
 const CHANNEL_ID =
     "UC1mpDsJIOLiw1e6wP-56Sgw";
